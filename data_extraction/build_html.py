ce934096240392720c873c1e0d20208ba39927b4
Paul Knoll <paul.knoll@hhi.fraunhofer.de>
2024-06-11 10:48:42 +0200

    create jinja html template

--- data_extraction/build_html.py
+++ data_extraction/build_html.py
@@ -1,6 +1,7 @@
 import os
 import pandas as pd
 import bibtexparser
+from jinja2 import Environment, FileSystemLoader
 
 def get_shortnames():
     #get shortnames from bibtex
@@ -18,11 +19,18 @@ def get_shortnames():
 
 def combine_tables_to_html():
     dfs = []
+    shortnames = get_shortnames()
     for file in os.listdir('results'):
         #read csvs
         df = pd.read_csv(f'results/{file}')
+        df['Submethod'] = df['Submethod'].astype('string').fillna('').replace('<NA>', '')
+
+        #combine Method and Submethods colum into new Method column, replace method name with shortname+submethod
+        df["Method"] = df["Method"].apply(lambda x: shortnames[x])
+        df["Method"] = df["Method"] + df["Submethod"]
+        df.drop(columns=["Submethod"], inplace=True)
         df.set_index("Method", inplace=True)
-        #remove colum "Data Source"
+        #remove colums "Data Source" and "Comment"
         df.drop(columns=["Data Source"], inplace=True)
         df.drop(columns=["Comment"], inplace=True)
 
@@ -38,10 +46,30 @@ def combine_tables_to_html():
     multi_col_df.reset_index(inplace=True)
     html_string = multi_col_df.to_html(na_rep='', index=False, table_id="results", classes=["display", "cell-border"], justify="center", border=0)
     
-    # with open("project-page/results.html", "w") as f:
-    #     f.write(html_string)
+    with open("project-page/results.html", "w") as f:
+        f.write(html_string)
     return html_string
 
 
+
 if __name__ == "__main__":
-    table = combine_tables_to_html()
+    results_table = combine_tables_to_html()
+
+    # Pfad zu deinem Template-Ordner
+    file_loader = FileSystemLoader('project-page')
+    env = Environment(loader=file_loader)
+
+    # Lade das Template
+    template = env.get_template('index_template.html')
+
+    # Daten, die in das Template eingefügt werden sollen
+    data = {
+        'results_table': results_table
+    }
+
+    # Render das Template mit den Daten
+    output = template.render(data)
+
+    # Speichere das gerenderte HTML in einer Datei
+    with open('project-page/index.html', 'w') as f:
+        f.write(output)
