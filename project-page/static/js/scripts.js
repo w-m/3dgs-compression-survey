ed0c86b7d54d4574362282b1c5272cc904125e14
paulbzm <paul.knolll@yahoo.de>
2024-07-30 15:16:06 +0200

    draw plots only after page load, draw psnr first, move color logic to build_html

--- project-page/static/js/scripts.js
+++ project-page/static/js/scripts.js
@@ -25,169 +25,167 @@ function resizePlots() {
     });
 }
 
-// Add event listener for window resize
-window.addEventListener('resize', resizePlots);
-
-var groupColors = Plotly.d3.scale.category20().range();
-
 var legendContainer = document.getElementById('legend');
-var groupNames = {};
 
-plotData.forEach((plotSet, i) => {
-    Object.keys(plotSet).forEach((key, j) => {
-        var plotInfo = plotSet[key];
-        var points = plotInfo.points;
-        var lines = [];
+function drawPlots(plotData, allowedKeys, allowedDatasets) {
+    plotData.forEach((plotSet, i) => {
+        if (allowedDatasets.includes(i)) {
+        
+        Object.keys(plotSet).forEach((key, j) => {
+            if (allowedKeys.includes(key)) {
+                var plotInfo = plotSet[key];
+                var points = plotInfo.points;
 
-        var groups = {};
-        points.forEach(point => {
-            if (!groups[point.group]) {
-                groups[point.group] = [];
-            }
-            groups[point.group].push(point);
-        });
-
-        var data = [];
-        Object.keys(groups).forEach(group => {
-            var groupPoints = groups[group];
-
-            if (!groupNames[group]) {
-                groupNames[group] = groupColors[groupPoints[0].group_id % groupColors.length];
-            }
-
-            data.push({
-                x: groupPoints.map(p => p.x),
-                y: groupPoints.map(p => p.y),
-                mode: 'markers',
-                text: groupPoints.map(p => p.label),
-                marker: {
-                    symbol: 'triangle-up',
-                    color: groupColors[groupPoints[0].group_id % groupColors.length],
-                    size: 12
-                },
-                name: group,
-                showlegend: false
-            });
-
-            if (groupPoints.length > 1) {
-                data.push({
-                    x: groupPoints.map(p => p.x),
-                    y: groupPoints.map(p => p.y),
-                    mode: 'lines',
-                    line: {
-                        color: groupColors[groupPoints[0].group_id % groupColors.length]
-                    },
-                    showlegend: false,
-                    hoverinfo: 'none'
+                var groups = {};
+                points.forEach(point => {
+                    if (!groups[point.group]) {
+                        groups[point.group] = [];
+                    }
+                    groups[point.group].push(point);
                 });
+
+                var data = [];
+                Object.keys(groups).forEach(group => {
+                    var groupPoints = groups[group];
+
+                    data.push({
+                        x: groupPoints.map(p => p.x),
+                        y: groupPoints.map(p => p.y),
+                        mode: 'markers',
+                        text: groupPoints.map(p => p.label),
+                        marker: {
+                            symbol: 'triangle-up',
+                            color: groupColors[group],
+                            size: 12
+                        },
+                        name: group,
+                        showlegend: false,
+                        visible: checkboxStates[group],
+                    });
+
+                    if (groupPoints.length > 1) {
+                        data.push({
+                            x: groupPoints.map(p => p.x),
+                            y: groupPoints.map(p => p.y),
+                            mode: 'lines',
+                            line: {
+                                color: groupColors[group]
+                            },
+                            showlegend: false,
+                            hoverinfo: 'none',
+                            visible: checkboxStates[group],
+                        });
+                    }
+                });
+
+                var horizontalLineHeight = plotInfo.lineHeight;
+
+                var layout = {
+                    title: plotInfo.title,
+                    xaxis: {
+                        title: plotInfo.xaxis,
+                        automargin: true,
+                        zeroline: false,
+                        // rangemode: "tozero",
+                    },
+                    yaxis: {
+                        title: {
+                            text: plotInfo.yaxis,
+                            // standoff: 5  // only applies to tab 1...?
+                        },
+                        automargin: true,
+                    },
+                    margin: {
+                        l: 65,
+                        r: 30,
+                        b: 70,
+                        t: 60,
+                        pad: 0
+                    },
+                    shapes: [],
+                    showlegend: false
+                };
+
+                if (horizontalLineHeight !== null) {
+                    layout.shapes.push({
+                        type: 'line',
+                        x0: 0,
+                        x1: 1,
+                        y0: horizontalLineHeight,
+                        y1: horizontalLineHeight,
+                        xref: 'paper',
+                        yref: 'y',
+                        line: {
+                            color: 'rgba(100, 100, 100, 0.75)',
+                            width: 2,
+                            dash: 'dash'
+                        }
+                    });
+                }
+
+                // Invert y-axis for the third tab
+                if (j === 2) {
+                    layout.yaxis.autorange = 'reversed';
+                }
+                var config = {
+                    displayModeBar: false
+                };
+                Plotly.newPlot(`plot${i}${j + 1}`, data, layout, config);
             }
         });
-
-        var horizontalLineHeight = plotInfo.lineHeight;
-
-        var layout = {
-            title: plotInfo.title,
-            xaxis: {
-                title: plotInfo.xaxis,
-                automargin: true,
-                zeroline: false,
-                rangemode: "tozero",
-            },
-            yaxis: {
-                title: {
-                    text: plotInfo.yaxis,
-                    // standoff: 5  // only applies to tab 1...?
-                },
-                automargin: true,
-            },
-            margin: {
-                l: 65,
-                r: 30,
-                b: 70,
-                t: 60,
-                pad: 0
-            },
-            shapes: [],
-            showlegend: false
-        };
-
-        if (horizontalLineHeight !== null) {
-            layout.shapes.push({
-                type: 'line',
-                x0: 0,
-                x1: 1,
-                y0: horizontalLineHeight,
-                y1: horizontalLineHeight,
-                xref: 'paper',
-                yref: 'y',
-                line: {
-                    color: 'rgba(100, 100, 100, 0.75)',
-                    width: 2,
-                    dash: 'dash'
-                }
-            });
-        }
-
-        // Invert y-axis for the third tab
-        if (j === 2) {
-            layout.yaxis.autorange = 'reversed';
-        }
-        var config = {
-            displayModeBar: false
-        };
-        Plotly.newPlot(`plot${i}${j + 1}`, data, layout, config);
+    }
     });
-});
+}
 
-// Add the legend item for 3DGS with a dashed line
-var legendItem3DGS = document.createElement('div');
-legendItem3DGS.className = 'legend-item';
+function drawLegend() {
 
-var dashedLine = document.createElement('div');
-dashedLine.style.width = '20px';
-dashedLine.style.height = '0';
-dashedLine.style.borderTop = '2px dashed rgba(100, 100, 100, 0.75)';
-dashedLine.style.marginRight = '5px';
+    // Add the legend item for 3DGS with a dashed line
+    var legendItem3DGS = document.createElement('div');
+    legendItem3DGS.className = 'legend-item';
 
-var labelText3DGS = document.createElement('p');
-labelText3DGS.innerText = '3DGS-30K';
-labelText3DGS.style.color = '#333333';
+    var dashedLine = document.createElement('div');
+    dashedLine.style.width = '20px';
+    dashedLine.style.height = '0';
+    dashedLine.style.borderTop = '2px dashed rgba(100, 100, 100, 0.75)';
+    dashedLine.style.marginRight = '5px';
 
-legendItem3DGS.appendChild(dashedLine);
-legendItem3DGS.appendChild(labelText3DGS);
-legendContainer.appendChild(legendItem3DGS);
+    var labelText3DGS = document.createElement('p');
+    labelText3DGS.innerText = '3DGS-30K';
+    labelText3DGS.style.color = '#333333';
 
-Object.keys(groupNames).forEach(group => {
-    var legendItem = document.createElement('div');
-    legendItem.className = 'legend-item';
+    legendItem3DGS.appendChild(dashedLine);
+    legendItem3DGS.appendChild(labelText3DGS);
+    legendContainer.appendChild(legendItem3DGS);
 
-    var checkbox = document.createElement('input');
-    checkbox.type = 'checkbox';
-    checkbox.checked = checkboxStates[group];
-    checkbox.dataset.group = group;
-    checkbox.style.marginLeft = '5px';
-    checkbox.style.width = '14px'; 
-    checkbox.style.height = '14px';
-    checkbox.addEventListener('change', updatePlotVisibility);
+    // Add rest of legend items
 
-    var colorBox = document.createElement('div');
-    colorBox.className = 'legend-color-box';
-    colorBox.style.backgroundColor = groupNames[group];
+    Object.keys(groupColors).forEach(group => {
+        var legendItem = document.createElement('div');
+        legendItem.className = 'legend-item';
 
-    var labelText = document.createElement('a');
-    labelText.href = groupLinks[group];
-    labelText.innerText = group;
+        var checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        checkbox.checked = checkboxStates[group];
+        checkbox.dataset.group = group;
+        checkbox.style.marginLeft = '5px';
+        checkbox.style.width = '14px'; 
+        checkbox.style.height = '14px';
+        checkbox.addEventListener('change', updatePlotVisibility);
 
-    legendItem.appendChild(colorBox);
-    legendItem.appendChild(labelText);
-    legendItem.appendChild(checkbox);
-    legendContainer.appendChild(legendItem);
-});
+        var colorBox = document.createElement('div');
+        colorBox.className = 'legend-color-box';
+        colorBox.style.backgroundColor = groupColors[group];
 
-// Call updatePlotVisibility for each checkbox to set initial visibility
-Object.keys(checkboxStates).forEach(group => {
-    updatePlotVisibility({target: {dataset: {group: group}, checked: checkboxStates[group]}});
-});
+        var labelText = document.createElement('a');
+        labelText.href = groupLinks[group];
+        labelText.innerText = group;
+
+        legendItem.appendChild(colorBox);
+        legendItem.appendChild(labelText);
+        legendItem.appendChild(checkbox);
+        legendContainer.appendChild(legendItem);
+    });
+}
 
 function updatePlotVisibility(event) {
     var group = event.target.dataset.group;
@@ -195,7 +193,7 @@ function updatePlotVisibility(event) {
 
     document.querySelectorAll('.plot-content').forEach(plotContent => {
         plotContent.data.forEach((trace, index) => {
-            if (trace.name === group || (trace.line && trace.line.color === groupNames[group])) {
+            if (trace.name === group || (trace.line && trace.line.color === groupColors[group])) {
                 Plotly.restyle(plotContent, { visible: visible }, [index]);
             }
         });
@@ -203,21 +201,13 @@ function updatePlotVisibility(event) {
 }
 
 
-var methodTable = document.getElementById('results');
-var methodRows = methodTable.getElementsByClassName('method-name');
+window.addEventListener('load', () => {
+    drawPlots(plotData, ["plot1"], [0,1,2,3]);
+    setTimeout(() => {drawPlots(plotData, ["plot2", "plot3"], [0,1,2,3]);}, 2000);
+    drawLegend();
 
-Array.from(methodRows).forEach(row => {
-    var methodName = row.getAttribute('data-method-name');
-    var color = groupNames[methodName];
+    // Add event listener for window resize
+    window.addEventListener('resize', resizePlots);
 
-    if (color) {
-        var colorBox = document.createElement('span');
-        colorBox.className = 'legend-color-box';
-        colorBox.style.backgroundColor = color;
-
-        var colorBoxContainer = row.querySelector('.legend-color-box-container');
-        if (colorBoxContainer) {
-            colorBoxContainer.insertBefore(colorBox, colorBoxContainer.firstChild);
-        }
-    }
 });
+
