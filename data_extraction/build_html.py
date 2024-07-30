7f6864462987b58169a1e1e5be1adc9d0ded734a
paulbzm <paul.knolll@yahoo.de>
2024-07-30 16:02:04 +0200

    include .webp images

--- data_extraction/build_html.py
+++ data_extraction/build_html.py
@@ -217,8 +217,10 @@ def load_methods_summaries(ranks):
                 title = f'<a href="{links[file.split(".")[0]]}" target="_blank">{title}</a>'
             summary = file_content.split('\n', 1)[1].strip()
 
-            #get image path, png or jpg
-            if os.path.exists(f"project-page/static/images/{file.split('.')[0]}.png"):
+            #get image path, webp, png or jpg
+            if os.path.exists(f"project-page/static/images/{file.split('.')[0]}.webp"):
+                image = f"static/images/{file.split('.')[0]}.webp"
+            elif os.path.exists(f"project-page/static/images/{file.split('.')[0]}.png"):
                 image = f"static/images/{file.split('.')[0]}.png"
             elif os.path.exists(f"project-page/static/images/{file.split('.')[0]}.jpg"):
                 image = f"static/images/{file.split('.')[0]}.jpg"
